2d673772a9d3163b46ba279cb09da700976f8645
rumiarif1009 <rumi.arif1009@gmail.com>
2026-02-09 13:53:22 +0530

    changes

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-019715e0d74f695be"  # Specify an appropriate AMI ID
+    ami           = "*"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
